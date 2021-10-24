f5e737c3a8f1ad8bd148144444de6051917e80b7
William Saylor <wsaylor@gmail.com>
2021-10-24 11:09:23 -0600

    Initial commit

---
+++ src/components/movie-list.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MovieList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Movie List</h1>
+            </div>
+        )
+    }
+}
